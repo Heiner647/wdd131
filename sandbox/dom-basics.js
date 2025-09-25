3253add0de965fb84a68cc0fc386d3e4742b7564
Heiner647 <seanheiner1@gmail.com>
2025-09-25 14:26:18 -0600

    week two prove

--- sandbox/dom-basics.js
+++ sandbox/dom-basics.js
@@ -5,6 +5,9 @@ document.body.appendChild(newParagraph);
 const newImage = document.createElement("img");
 newImage.setAttribute("src", "https://picsum.photos/200");
 newImage.setAttribute("alt", "Random image from picsum.");
+// alturntive
+// newImage.src = "https://picsum.photos/200";
+// newImage.alt = "Random image from picsum.";
 document.body.appendChild(newImage);
 
 const newDiv = document.createElement("div");
@@ -12,11 +15,13 @@ newDiv.innerHTML = "<ul><li>One</li><li>Two</li><li>Three</li></ul>";
 document.body.appendChild(newDiv);
 
 const newSection = document.createElement('section');
+document.body.appendChild(newSection);
+
 const newH2 = document.createElement("h2");
 newH2.innerText = "DOM Basices";
 newSection.appendChild(newH2);
+
 const newP = document.createElement("p")
 newP.innerText = "This was added through Javascript";
 newSection.appendChild(newP);
 
-document.body.appendChild(newSection);
