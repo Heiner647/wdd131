bacc72a96df86f2e4f7d6e9fcec720f3418f1ca1
Heiner647 <seanheiner1@gmail.com>
2025-10-09 15:31:26 -0600

    prove

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -28,4 +28,15 @@ modal.addEventListener('click', (event) => {
         modal.close();
     }
 });
+
+
+
+const menuBtn = document.querySelector('.menu-btn');
+const navLinks = document.querySelector('nav');
+
+function toggleMenu() {
+    navLinks.classList.toggle('hidden');
+}
+menuBtn.addEventListener('click', toggleMenu);
+
           
