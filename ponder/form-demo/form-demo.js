da2e331925c0de169abf926ea8ba74e8dd1e530a
Heiner647 <seanheiner1@gmail.com>
2025-10-30 14:33:38 -0600

    add form-demo

--- ponder/form-demo/form-demo.js
+++ ponder/form-demo/form-demo.js
@@ -22,28 +22,43 @@ function validateForm(event) {
   }
 }
 
-function togglePaymentDetails(e) {
-  // get a reference to the form. We can access all the named form inputs through the form element.
-  const theForm = ;
-  // we will also need the creditCardContainer and paypalUsernameContainer
-  const creditCardContainer = ;
-  const paypalContainer = ;
-
-  // Hide payment containers by adding the '.hide' class to each of them
-
-  // Disable required for payment fields...if we hide a required field the browser will throw an error when we try to submit!
-
-
-  // Show the container based on the selected payment method, and add the required attribute back.
-
-}
-
 // helper function to display our errors.
 function showErrors(errors) {
   const errorEl = document.querySelector(".errors");
   const html = errors.map((error) => `<p>${error}</p>`);
   errorEl.innerHTML = html.join("");
 }
-// attach a change event handler to the paymentMethod input
 
 // attach a submit event handler to the form
+
+function togglePaymentDetails(e) {
+  // get a reference to the form. We can access all the named form inputs through the form element.
+  const theForm = document.querySelector("#checkoutForm");
+  // we will also need the creditCardContainer and paypalUsernameContainer
+  const creditCardContainer = document.getElementById(
+    "creditCardNumberContainer"
+  );
+  const paypalContainer = document.getElementById("paypalUsernameContainer");
+
+  // Hide payment containers
+  creditCardContainer.classList.add("hide");
+  paypalContainer.classList.add("hide");
+  // Disable required for the hidden fields...if we hide a required field the browser will throw an error when we try to submit!
+  theForm.creditCardNumber.required = false;
+  // theForm.paypalUsername.required = false;
+  paypalContainer.removeAttribute("required");
+
+  // Show the container based on the selected payment method
+  if (theForm.paymentMethod.value === "creditCard") {
+    creditCardContainer.classList.remove("hide");
+    theForm.creditCardNumber.required = true;
+  } else if (theForm.paymentMethod.value === "paypal") {
+    paypalContainer.classList.remove("hide");
+    theForm.paypalUsername.required = true;
+  }
+}
+
+// attach a change event handler to the paymentMethod input
+document
+  .querySelector("#paymentMethod")
+  .addEventListener("change", togglePaymentDetails);
